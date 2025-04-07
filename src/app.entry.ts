4e1977430372a2a2abe60bba509abc5a6b177a5b
rlimbu_gan <rlimbu@gameaccount.com>
2025-04-07 16:00:47 +0100

    add: sounds and with pixi/sound plugin

--- src/app.entry.ts
+++ src/app.entry.ts
@@ -4,6 +4,7 @@ import { ResizerService } from "./core/resizer/resizer.service";
 import { SceneService } from "./core/scene/scene.service";
 import { SceneModuleOptions } from "./core/scene/scene.providers";
 import { KeyboardService } from "./core/keyboard/keyboard.service";
+import { sound } from "@pixi/sound";
 
 export const AppEntry = createProvider(async ({ inject }) => {
     const assets = inject(AssetsService);
@@ -17,6 +18,8 @@ export const AppEntry = createProvider(async ({ inject }) => {
     
     await assets.loadManifest("./manifest.json");
     await assets.switchBundle("default");
+
+	sound.play('music', {loop: true, volume: 0.25});
 
     scene.run(defaultScene);
 })
